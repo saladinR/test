4bde40e726fff42eca5c97eb4e36fcbf63766a69
saladinR <salaheddine.raiss@gmail.com>
2021-12-30 16:32:12 +0100

    add csrf

--- resources/views/file-upload.blade.php
+++ resources/views/file-upload.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 
     <title>Laravel File Upload</title>
